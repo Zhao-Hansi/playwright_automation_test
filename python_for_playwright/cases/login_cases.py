0b78ab54babcc27edaaa4929c72bc9fdedb6a755
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-10-10 14:28:17 +0800

    refactor the test with python

---
+++ python_for_playwright/cases/login_cases.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page, expect
+import re
+from python_for_playwright.test_data import login_page_locator as login_page
+
+
+def test_example(page: Page):
+    page.goto("https://litemall.hogwarts.ceshiren.com/#/login")
+    page.get_by_placeholder(login_page.login_page.username).fill(login_page.login_page.right_username)
+    page.get_by_placeholder(login_page.login_page.password).click()
+    page.get_by_placeholder(login_page.login_page.password).fill(login_page.login_page.right_password)
+    page.get_by_role(login_page.login_page.login_button).click()
+    page.locator("div").filter(has_text=re.compile(r"^商场管理$")).click()
+    page.get_by_role("link", name="行政区域").click()
+    page.get_by_role("cell", name=" 北京市").locator("i").click()
+    page.locator("#tags-view-container").get_by_text("首页").click()
---
+++ python_for_playwright/test_data/login_page_locator.py
@@ -0,0 +1,6 @@
+class login_page:
+    username = '管理员账户'
+    password = '管理员密码'
+    login_button = 'button[name="登录"]'
+    right_username = 'hogwarts'
+    right_password = 'test12345'
