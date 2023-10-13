09cb57cc69ea380bd74cb6163e9be722af980518
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-10-13 17:07:00 +0800

    use fixture take screenshot when case is failed

--- python_version_playwright/cases/test_login_cases.py
+++ python_version_playwright/cases/test_login_cases.py
@@ -4,11 +4,10 @@ import pytest
 from playwright.sync_api import Page
 
 from python_version_playwright.pages import login_page
-from python_version_playwright.common_function.common import open_page
+from python_version_playwright.common_function.common import open_page, take_screenshot
 
 
-def test_example(open_page, page: Page):
-    # page.goto("https://litemall.hogwarts.ceshiren.com/#/login")
+def test_example(take_screenshot, open_page, page: Page):
     page.get_by_placeholder(login_page.login_page_locator.username).fill(login_page.login_page_locator.right_username)
     page.get_by_placeholder(login_page.login_page_locator.password).click()
     page.get_by_placeholder(login_page.login_page_locator.password).fill(login_page.login_page_locator.right_password)
@@ -16,10 +15,9 @@ def test_example(open_page, page: Page):
     page.locator("div").filter(has_text=re.compile(r"^商场管理$")).click()
     page.get_by_role("link", name="行政区域").click()
     page.get_by_role("cell", name=" 北京市").locator("i").click()
-    page.locator("#tags-view-container").get_by_text("首页").click()
+    page.locator("#tags-view-container").get_by_text("首324页").click()
 
 
 @pytest.mark.parametrize("username,password", [("hogwarts", "test12345")])
 def test_cases_for_login(open_page, page: Page, username, password):
-    page.goto("https://litemall.hogwarts.ceshiren.com/#/login")
     login_page.login(page, username, password)
--- python_version_playwright/common_function/common.py
+++ python_version_playwright/common_function/common.py
@@ -11,3 +11,17 @@ def test_login_success(open_page):
     # 使用open_page fixture打开登录页面
     # 在这里执行登录测试逻辑
     pass
+
+
+@pytest.fixture(scope='function')
+def take_screenshot(request, page):
+    yield
+    if request.node.rep_call.failed:
+        test_name = request.node.name
+        screenshot_path = f"python_version_playwright/screenshots/{test_name}.png"
+        page.screenshot(path=screenshot_path, full_page=True)
+
+
+@pytest.fixture
+def record_video(page: Page):
+    pass
